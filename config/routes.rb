e385f8c97a6ae69ae3327d311588135c45780d3c
wfjeff <wm.jeffries@gmail.com>
2014-05-10 13:24:29 -0400

    update cemissions route in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#index'
-  get 'application#cemissions'
+  get 'cemissions' => 'application#cemissions'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
