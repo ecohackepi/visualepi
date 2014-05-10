305df9404d76509309879e7dc7663c86c922568d
wfjeff <wm.jeffries@gmail.com>
2014-05-10 17:14:04 -0400

    add radar chart json call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def radar_chart
+    respond_to do |f|
+      f.json {render json: Country.radar_chart.to_json.html_safe }
+    end
+  end
+
+  private
+    def radar_chart_params
+      permit("year", "country")
+    end
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,29 @@
 class Country < ActiveRecord::Base
+
+  CATEGORIES = ["EH_AirQuality", "EH_HealthImpacts", "EH_WaterSanitation", "EV_Agriculture", "EV_BiodiversityHabitat", "EV_ClimateEnergy", "EV_Fisheries", "EV_Forests", "EV_WaterResources"]
+  def self.new_from_params(params)
+    year = params["year"]
+    year = params["country"]
+  end
+
+  def self.radar_chart
+    {
+      "countries" => country_categories
+    }
+  end
+
+  def self.country_categories
+    all.map do |country|
+      {
+        "name" => country.iso,
+        "categories" => country.categories
+      }
+    end
+  end
+
+  def categories
+    CATEGORIES.map do |cat|
+      {"name" => cat, "value" => send(cat)}
+    end
+  end
 end
