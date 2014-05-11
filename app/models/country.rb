649bff45d24e8d47a41b4f0ebb751996c8f6932d
wfjeff <wm.jeffries@gmail.com>
2014-05-11 12:07:04 -0400

    add support for multiple years

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,7 @@ class Country < ActiveRecord::Base
 
   INDICATOR_NAMES = ["EH_AirQuality", "EH_HealthImpacts", "EH_WaterSanitation", "EV_Agriculture", "EV_BiodiversityHabitat", "EV_ClimateEnergy", "EV_Fisheries", "EV_Forests", "EV_WaterResources"]
   COUNTRY_NAMES = Country.pluck(:country)
-  YEARS = [2012]
+  YEARS = (2002..2012).to_a
 
   def self.countries_from_params(params)
     params['countries'] || []
@@ -12,16 +12,18 @@ class Country < ActiveRecord::Base
     country_names = params['countries'] || COUNTRY_NAMES
     indicator_names = params['indicators'] || INDICATOR_NAMES
     years = params['years'] || YEARS
-    country_names.map do |country_name|
-      country = Country.find_by(country: country_name.downcase.capitalize)
-        {
-          "year" => "2012",
-          "data" => {
-            "id" => country.iso,
-            "name" => country_name,
-            "indicators" => country.indicators(indicator_names)
+    years.map do |year|  
+      country_names.map do |country_name|
+        country = Country.find_by(year: year.to_s, country: country_name.downcase.capitalize)
+          {
+            "year" => year.to_s,
+            "data" => {
+              "id" => country.iso,
+              "name" => country_name,
+              "indicators" => country.indicators(indicator_names)
+            }
           }
-        }
+      end
     end
   end
 
